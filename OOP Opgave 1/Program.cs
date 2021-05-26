95b45e301cc91c0108ab373a4a63e08bad4d8c4a
danielhoyerhare@gmail.com <danielhoyerhare@gmail.com>
2021-05-26 10:24:57 +0200

    OOP 1

--- OOP Opgave 1/Program.cs
+++ OOP Opgave 1/Program.cs
@@ -10,7 +10,7 @@ namespace OOP_Opgave_1
             Console.WriteLine(NewCar.GetLicense() + ", " + NewCar.GetBrand());
             Car NewCar2 = new Car("GE97374", "Lamborghini", new DateTime(2009,11,10));
             Console.WriteLine(NewCar2.GetLicense() + ", " + NewCar2.GetBrand() + ", " + NewCar2.GetAge());
-            Console.ReadKey(); //292929
+            Console.ReadKey(); 
         }
     }
 }
