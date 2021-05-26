9e4c0898dcba44e8525b4fdaa5634abd7509b9d9
danielhoyerhare@gmail.com <danielhoyerhare@gmail.com>
2021-05-26 08:33:47 +0200

    Add project files.

---
+++ OOP Opgave 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Opgave_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
