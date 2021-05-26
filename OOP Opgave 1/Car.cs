2a545ce4e42f9fe638e5ba589b7ea5ad70e32e71
danielhoyerhare@gmail.com <danielhoyerhare@gmail.com>
2021-05-26 11:37:20 +0200

    OOP 2

--- OOP Opgave 1/Car.cs
+++ OOP Opgave 1/Car.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace OOP_Opgave_1
 {
-    class Car
+    class Car: MotorVehicle
     {
         private string licenseplate;
         private string brand;
-        private int age;
-        private DateTime year;
         public string GetLicense()
         {
             return licenseplate;
@@ -28,24 +26,6 @@ namespace OOP_Opgave_1
         public void SetBrand(string brandInput)
         {
             brand = brandInput;
-        }
-        public int GetAge()
-        {
-            return age;
-        }
-        public void SetAge(int ageInput)
-        {
-            age = ageInput;
-        }
-        public void SetYear(DateTime yearInput)
-        {
-            year = yearInput;
-            AgeCounter NewCarAge = new AgeCounter(year);
-            SetAge(NewCarAge.GetCarAge(year));
-        }
-        public string GetYear()
-        {
-            return year.ToString("dd/MM/yyyy");
         }
         public Car(string licenseInput, string brandInput)
         {
---
+++ OOP Opgave 1/MotorVehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Opgave_1
+{
+    abstract class MotorVehicle
+    {
+        record OwnerContactInfo(string name, string telNo);
+        private string color;
+        private string motorsize;
+        private int age;
+        private DateTime year;
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetAge(int ageInput)
+        {
+            age = ageInput;
+        }
+        public void SetYear(DateTime yearInput)
+        {
+            year = yearInput;
+            AgeCounter NewCarAge = new AgeCounter(year);
+            SetAge(NewCarAge.GetCarAge(year));
+        }
+        public string GetYear()
+        {
+            return year.ToString("dd/MM/yyyy");
+        }
+        public abstract void SetOwnerInfo(string name, string telNo);
+    }
+}
